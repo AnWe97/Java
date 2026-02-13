a272032c4e035e3caa5ea9606a7441556c81bfc3
AnWe97 <a.wedemeier@web.de>
2026-02-13 13:26:50 +0100

    Add files via upload

---
+++ PasswordManager/src/main/java/org/example/User.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class User {
+
+    String username;
+    String password;
+
+    public User(String username, String password){
+
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+}
